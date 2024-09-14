a74ab2bca06612d01b1b380466b16a8957cf14e7
xa2383 <167450938+xa2383@users.noreply.github.com>
2024-09-14 09:22:20 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX 10
+
+// Function declarations (prototypes)
+void push(int stack[], int *p, int data);
+int pop(int stack[], int *p);
+
 int main()
 {
     int stack[MAX], TOP = MAX, data1, data2, res, i;
     char postfix[20];
+    
     printf("Enter postfix expression\n");
     scanf("%s", postfix);
+    
     for (i = 0; postfix[i] != '\0'; i++)
     {
         switch (postfix[i])
@@ -39,15 +46,18 @@ int main()
             push(stack, &TOP, postfix[i] - '0');
         }
     }
+    
     res = pop(stack, &TOP);
-    printf("Result of computation:%d", x);
+    printf("Result of computation: %d\n", res);  // Changed 'x' to 'res'
     return 0;
 }
+
 void push(int stack[], int *p, int data)
 {
     *p = *p - 1;
     stack[*p] = data;
 }
+
 int pop(int stack[], int *p)
 {
     int data;
