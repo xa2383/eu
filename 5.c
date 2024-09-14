6f6a0e970700c9dac3001533b09067d3605817ed
xa2383 <167450938+xa2383@users.noreply.github.com>
2024-09-14 09:29:58 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,19 +1,15 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
-struct complex
-{
+struct complex {
     int real, img;
 };
 
-int main()
-{
+int main() {
     int choice, x, y, z;
     struct complex a, b, c;
 
-    while (1)
-    {
+    while (1) {
         printf("Press 1 to add two complex numbers.\n");
         printf("Press 2 to subtract two complex numbers.\n");
         printf("Press 3 to multiply two complex numbers.\n");
@@ -22,11 +18,11 @@ int main()
         printf("Enter your choice:\n");
         scanf("%d", &choice);
 
-        if (choice == 5)
+        if (choice == 5) {
             exit(0);
+        }
 
-        if (choice >= 1 && choice <= 4)
-        {
+        if (choice >= 1 && choice <= 4) {
             printf("Enter a and b where a + ib is the first complex number.\n");
             printf("\na = ");
             scanf("%d", &a.real);
@@ -40,97 +36,49 @@ int main()
             scanf("%d", &b.img);
         }
 
-        // Operations for addition, subtraction, multiplication, and division would go here
+        if (choice == 1) {
+            // Addition of two complex numbers
+            c.real = a.real + b.real;
+            c.img = a.img + b.img;
+            if (c.img >= 0)
+                printf("Sum of the complex numbers = %d + %di\n", c.real, c.img);
+            else
+                printf("Sum of the complex numbers = %d %di\n", c.real, c.img);
+        } else if (choice == 2) {
+            // Subtraction of two complex numbers
+            c.real = a.real - b.real;
+            c.img = a.img - b.img;
+            if (c.img >= 0)
+                printf("Difference of the complex numbers = %d + %di\n", c.real, c.img);
+            else
+                printf("Difference of the complex numbers = %d %di\n", c.real, c.img);
+        } else if (choice == 3) {
+            // Multiplication of two complex numbers
+            c.real = a.real * b.real - a.img * b.img;
+            c.img = a.real * b.img + a.img * b.real;
+            if (c.img >= 0)
+                printf("Multiplication of the complex numbers = %d + %di\n", c.real, c.img);
+            else
+                printf("Multiplication of the complex numbers = %d %di\n", c.real, c.img);
+        } else if (choice == 4) {
+            // Division of two complex numbers
+            if (b.real == 0 && b.img == 0) {
+                printf("Division by 0 + 0i isn't allowed.\n");
+            } else {
+                x = a.real * b.real + a.img * b.img;
+                y = a.img * b.real - a.real * b.img;
+                z = b.real * b.real + b.img * b.img;
+                c.real = x / z;
+                c.img = y / z;
+                if (c.img >= 0)
+                    printf("Division of the complex numbers = %d + %di\n", c.real, c.img);
+                else
+                    printf("Division of the complex numbers = %d %di\n", c.real, c.img);
+            }
+        } else {
+            printf("Invalid choice.\n");
+        }
     }
+
     return 0;
-}
-if (choice == 1)
-{
-    c.real = a.real + b.real;
-    c.img = a.img + b.img;
-    if (c.img >= 0)
-        printf("Sum of the complex numbers = %d + %di\n", c.real, c.img);
-    else
-        printf("Sum of the complex numbers = %d %di\n", c.real, c.img);
-}
-
-else if (choice == 2)
-{
-    c.real = a.real - b.real;
-    c.img = a.img - b.img;
-    if (c.img >= 0)
-        printf("Difference of the complex numbers = %d + %di\n", c.real, c.img);
-    else
-        printf("Difference of the complex numbers = %d %di\n", c.real, c.img);
-}
-
-else if (choice == 3)
-{
-    c.real = a.real * b.real - a.img * b.img;
-    c.img = a.real * b.img + a.img * b.real;
-    if (c.img >= 0)
-        printf("Multiplication of the complex numbers = %d + %di\n", c.real, c.img);
-    else
-        printf("Multiplication of the complex numbers = %d %di\n", c.real, c.img);
-}
-
-else if (choice == 4)
-{
-    if (b.real == 0 && b.img == 0)
-        printf("Division by 0 + 0i isn't allowed.\n");
-    else
-    {
-        x = a.real * b.real + a.img * b.img;
-        y = a.img * b.real - a.real * b.img;
-        z = b.real * b.real + b.img * b.img;
-        c.real = x / z;
-        c.img = y / z;
-        if (c.img >= 0)
-            printf("Division of the complex numbers = %d + %di\n", c.real, c.img);
-        else
-            printf("Division of the complex numbers = %d %di\n", c.real, c.img);
-    }
-}
-}
-
-return 0;
-}
-b.img *b.img;
-if (x % z == 0 && y % z == 0)
-{
-    if (y / z >= 0)
-        printf("Division of the complex numbers = %d/%d + %d/%di\n", x / z, y / z);
-    else
-        printf("Division of the complex numbers = %d/%d %d/%di\n", x / z, y / z);
-}
-else if (x % z == 0 && y % z != 0)
-{
-    if (y / z >= 0)
-        printf("Division of two complex numbers = %d/%d + %d/%di\n", x / z, y / z);
-    else
-        printf("Division of two complex numbers = %d/%d %d/%di\n", x / z, y / z);
-}
-else if (x % z != 0 && y % z == 0)
-{
-    if (y / z >= 0)
-        printf("Division of two complex numbers = %d/%d + %d/%di\n", x / z, y / z);
-    else
-        printf("Division of two complex numbers = %d/%d %d/%di\n", x / z, y / z);
-}
-else
-{
-    if (y / z >= 0)
-        printf("Division of two complex numbers = %d/%d + %d/%di\n", x, y, z);
-    else
-        printf("Division of two complex numbers = %d/%d %d/%di\n", x, y, z);
-}
-}
-else
-{
-    printf("Invalid choice.\n");
-    printf("\nPress any key to enter choice again...\n");
-}
-}
-
-return 0;
 }
